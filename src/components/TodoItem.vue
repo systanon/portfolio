15e39da5b36bb772fcbf7f77a66e390696d8518c
Serhii <serhii.tustanovskyi88@gmail.com>
2025-06-03 11:52:11 +0200

    feat: add todo item

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,68 @@
+<template>
+  <div ref="todo" class="todo-item">
+    <div class="todo-item__checked">
+      <input
+        type="checkbox"
+        :checked="todo.completed"
+        @input="
+          $emit('completeHandler', {
+            id: todo._id,
+            payload: { completed: !todo.completed },
+          })
+        "
+      />
+    </div>
+
+    <div class="todo-item__info">
+      <h2 class="todo-item__title">{{ todo.title }}</h2>
+      <p class="todo-item__description">{{ todo.description }}</p>
+    </div>
+    <div class="todo-item__actions">
+      <button @click="$emit('editHandler', todo)">edit</button>
+      <button @click="$emit('deleteHandler', todo)">remove</button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "TodoItem",
+  props: {
+    todo: {
+      type: Object,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.todo-item {
+  box-sizing: border-box;
+  padding: 4rem;
+  border-radius: 2rem;
+  border: black solid 1px;
+  display: flex;
+  gap: 1rem;
+  width: 30rem;
+  min-height: 10rem;
+  &__checked {
+    display: flex;
+    align-items: center;
+    flex: 0 0;
+  }
+  &__info {
+    flex: 1 1;
+  }
+  &__actions {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+    flex: 0 0;
+  }
+  &__title {
+    margin: 0;
+  }
+}
+</style>
--- src/views/TodosView.vue
+++ src/views/TodosView.vue
@@ -1,19 +1,27 @@
 <template>
-  <h1>This is Todos page</h1>
-  <ul v-if="todos.length">
-    <li v-for="todo in todos" :key="todo.id">{{ todo.title }}</li>
-  </ul>
-  <p v-else>Epmty todos</p>
+  <h2>This is Todos page</h2>
+  <section class="todos-view">
+  <TodoItem v-for="todo in todos" :key="todo.id" :todo="todo" />
+  <p v-if="!todos.length">Epmty todos</p>
+  </section>
 </template>
 
 <script setup lang="ts">
 import { onMounted } from "vue";
 import { storeToRefs } from "pinia";
 import { useTodoStore } from "@/plugins/store/todos";
-
+import TodoItem from "@/components/TodoItem.vue";
 const todoStore = useTodoStore();
 const { todos } = storeToRefs(todoStore);
 const { init } = todoStore;
 
 onMounted(init);
 </script>
+
+<style scoped lang="scss">
+.todos-view {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 5px;
+}
+</style>
