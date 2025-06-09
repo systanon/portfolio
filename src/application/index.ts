58b67840658041134b87ddb24fd80089c4e6aafa
Serhii <tustanovskiis@gmail.com>
2025-06-09 22:31:25 +0200

    feat: add interceptor

--- src/application/index.ts
+++ src/application/index.ts
@@ -1,4 +1,4 @@
-import { HTTPClient } from "../lib/http.client";
+import { HTTPClient, type ResponseWithRequest } from "../lib/http.client";
 
 import { TodoService } from "./services/todo.service";
 
@@ -15,6 +15,48 @@ export const httpClient = new HTTPClient({
 
 export const todoService = new TodoService(httpClient);
 export const authService = new AuthService(httpClient);
+httpClient.interceptors.request.use(
+  (request) => {
+    if (request.credentials === "include") {
+      const newToken = localStorage.getItem("access_token");
+      const newHeaders = new Headers(request.headers);
+      if (newToken) {
+        newHeaders.set("Authorization", newToken);
+      }
+      (request as any).headers = newHeaders;
+      return request
+    }
+    return request
+  },
+  async (reason) => {
+    return Promise.reject(reason)
+  },
+  { runWhen: () => true }
+)
+httpClient.interceptors.response.use(
+  async (response) => {
+    if (response.status !== 401) return response;
+
+    const originalRequest = (response as ResponseWithRequest).request;
+
+    const alreadyTried = (originalRequest as any).data.retry
+    if (alreadyTried) return response;
+
+    try {
+      await authService.refresh();
+      
+      (originalRequest as any).data.retry = true;
+
+      return await httpClient.do(originalRequest.resource, originalRequest)
+    } catch (e) {
+      return response;
+    }
+  },
+  async (reason) => {
+    return Promise.reject(reason)
+  },
+  { runWhen: () => true }
+)
 
 export const createApplication = (): Application => new Application(todoService, authService);
 
