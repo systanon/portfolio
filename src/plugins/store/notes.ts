e8c10a144ecc1b47bdb0392883ba5f5d9ba75924
Serhii <tustanovskiis@gmail.com>
2025-06-16 15:30:18 +0200

    feat: create note ui

---
+++ src/plugins/store/notes.ts
@@ -0,0 +1,72 @@
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+
+import { application } from '../../application'
+import { AppError } from '../../types/app-errors';
+import type { CreateNoteDTO, Note, UpdateNoteDTO } from '../../types/notes';
+import type { GetAllParams } from '@/types/app.types';
+
+
+export const useNotesStore = defineStore('notes', () => {
+
+  const notes = ref<Note[]>([])
+  const notesMap = ref(new Map<number, Note>())
+  const total = ref<number>(0)
+  const pages = ref<number>(1)
+
+  async function getAll(params: GetAllParams) {
+    try {
+      const { data, total: _total, pages: _pages } = await application.getAllNotes(params)
+      notes.value = data
+      notesMap.value = new Map(data.map(note => [note.id, note]))
+      total.value = _total
+      pages.value = _pages
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  async function update(_id: number, payload: UpdateNoteDTO) {
+    const _payload = { ...payload }
+    const res = await application.updateNote(_id, payload)
+    if (res instanceof AppError) {
+      console.warn(res)
+    } else {
+      const note = notes.value.find(t => t.id === _id)
+      if (note) Object.assign(note, _payload)
+
+      const mapTodo = notesMap.value.get(_id)
+      mapTodo && Object.assign(mapTodo, _payload)
+    }
+  }
+
+  async function create(payload: CreateNoteDTO) {
+    const res = await application.createNote(payload)
+    if (res instanceof AppError) {
+      console.warn(res)
+    } else {
+      const note = await application.getOneNote(res)
+      if (note instanceof AppError) {
+        console.warn(res)
+      } else {
+        notes.value.push(note)
+        notesMap.value.set(note.id, note)
+      }
+    }
+  }
+
+  async function remove(id: number) {
+    const res = await application.deleteNote(id)
+    if (res instanceof AppError) {
+      console.warn(res)
+    } else {
+      const index = notes.value.findIndex((note) => note.id === id)
+      if (index !== -1) {
+        notes.value.splice(index, 1)
+      }
+      notesMap.value.delete(id)
+    }
+  }
+
+  return { getAll, notes, notesMap, update, create, remove, total, pages }
+})
--- src/views/NotesView.vue
+++ src/views/NotesView.vue
@@ -1,3 +1,186 @@
 <template>
-  <h2>Notes view</h2>
+  <h2>This is Notes page</h2>
+  <button @click="createHandler">Create Note</button>
+  <section class="page-note__notes">
+    <NoteItem
+      v-for="[id, note] of [...notesMap.entries()]"
+      :key="id"
+      :note="note"
+      @editHandler="editHandler"
+      @deleteHandler="deleteHandler"
+    />
+
+    <p v-if="!notes.length">Epmty notes</p>
+  </section>
+
+  <UIModal ref="deleteModalRef" title="Delete note?" class="page-note__modal">
+    <template #default>
+      <div class="page-note__modal-form delete-note-form">
+        <h3>Are you sure you want to delete note?</h3>
+      </div>
+    </template>
+  </UIModal>
+  <UIModal ref="editModalRef" title="Update note" class="page-note__modal">
+    <template #default>
+      <div class="page-note__modal-form update-note-form">
+        <h3>Edit note</h3>
+        <input type="text" v-model="note.title" />
+        <input type="text" v-model="note.description" />
+      </div>
+    </template>
+  </UIModal>
+  <UIModal ref="createModalRef" title="Create Note" class="page-note__modal">
+    <template #default>
+      <div class="page-note__modal-form update-note-form">
+        <h3>Create note</h3>
+        <input type="text" v-model="note.title" />
+        <input type="text" v-model="note.description" />
+      </div>
+    </template>
+  </UIModal>
+
+  <UIPagination
+    v-model:page="pagination.page"
+    v-model:pages="pagination.pages"
+    @first-page="firstPage"
+    @prev-page="prevPage"
+    @next-page="nextPage"
+    @latest-page="latestPage"
+    @btn-page="btnPage"
+  />
 </template>
+
+<script setup lang="ts">
+  import { onMounted, ref, reactive, computed, watch } from 'vue';
+  import { APP_CONFIG } from '@/constants';
+  import { useRoute, useRouter } from 'vue-router';
+  import { storeToRefs } from 'pinia';
+  import { useNotesStore } from '@/plugins/store/notes';
+  import NoteItem from '@/components/NoteItem.vue';
+  import { usePagination } from '@/hooks/pagination';
+  import UIPagination from '@/components/UiPagination.vue';
+
+  import UIModal, { type IModalOpen } from '@/components/UiModal.vue';
+  import { type UpdateNoteDTO, type ReplaceNoteDTO, type Note } from '../types/notes';
+
+  const note = reactive({
+    title: '',
+    description: '',
+  } as ReplaceNoteDTO);
+
+  const { DEFAULT_PAGE, DEFAULT_PAGE_SIZE } = APP_CONFIG;
+
+  const deleteModalRef = ref<IModalOpen | null>(null);
+  const editModalRef = ref<IModalOpen | null>(null);
+  const createModalRef = ref<IModalOpen | null>(null);
+
+  const route = useRoute();
+  const router = useRouter();
+
+  const notesStore = useNotesStore();
+  const { pagination, firstPage, prevPage, nextPage, latestPage, btnPage, setPages } =
+    usePagination(DEFAULT_PAGE_SIZE);
+  const { notes, notesMap, pages } = storeToRefs(notesStore);
+  const { getAll, update, create, remove } = notesStore;
+
+  const requestParams = computed(() => {
+    const { perPage, page } = pagination;
+    return {
+      perPage,
+      page,
+    };
+  });
+
+  const deleteHandler = async (note: Note) => {
+    const { id } = note;
+    const modal = deleteModalRef.value;
+    const res = await modal?.open();
+    if (res) remove(id);
+  };
+
+  const editHandler = async (_note: Note) => {
+    const { id } = _note;
+    fillInputs(_note);
+    const modal = editModalRef.value;
+    const res = await modal?.open();
+    if (res) {
+      update(id, note);
+    }
+    clearInputs();
+  };
+
+  const parsePouterQuery = () => {
+    const { page, perPage } = route.query;
+    pagination.page = Number(page) || DEFAULT_PAGE;
+    pagination.perPage = Number(perPage) || DEFAULT_PAGE_SIZE;
+  };
+
+  const saveRouterQuery = () => {
+    const query = { ...route.query, ...requestParams.value };
+    router.replace({ query });
+  };
+
+  const createHandler = async () => {
+    const modal = createModalRef.value;
+    const res = await modal?.open();
+    if (res) {
+      create(note);
+    }
+    clearInputs();
+  };
+
+  const completeHandler = ({ id, payload }: { id: number; payload: UpdateNoteDTO }) => {
+    update(id, payload);
+  };
+
+  const clearInputs = () => {
+    note.title = '';
+    note.description = '';
+  };
+
+  const fillInputs = (_note: UpdateNoteDTO) => {
+    note.title = _note.title ?? '';
+    note.description = _note.description ?? '';
+  };
+
+  watch(
+    pages,
+    (pages) => {
+      if (!pages) return;
+      setPages(pages);
+    },
+    { immediate: true },
+  );
+  watch(
+    requestParams,
+    (params) => {
+      getAll(params);
+      saveRouterQuery();
+    },
+    { immediate: true },
+  );
+
+  onMounted(() => {
+    parsePouterQuery();
+    getAll(requestParams.value);
+  });
+</script>
+
+<style scoped lang="scss">
+  .page-note {
+    &__notes {
+      display: flex;
+      flex-wrap: wrap;
+      gap: 5px;
+    }
+
+    &__modal-form {
+      display: flex;
+      flex-direction: column;
+      gap: 0.5rem;
+      width: 40vw;
+      min-width: 400px;
+      max-width: 600px;
+    }
+  }
+</style>
