b0c63a8cd1228b28f84be128dee266ea87f4e0a1
Serhii <tustanovskiis@gmail.com>
2025-06-18 22:14:20 +0200

    feat: download svg

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,42 @@
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
+import { createHtmlPlugin } from 'vite-plugin-html'
+import { resolve } from 'node:path'
+import { getSVGSymbolsString } from './scripts/svg'
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(async ({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   const isDocker = process.env.DOCKER_DEV === 'true'
   const target = isDocker
     ? env.VITE_APP_HOST_URL_DEV
     : env.VITE_APP_HOST_URL
   return {
-    plugins: [vue()],
+    plugins: [vue(), createHtmlPlugin({
+      minify: true,
+      entry: 'src/main.ts',
+      template: 'index.html',
+      inject: {
+        data: {
+          injectSVGSymbols: await getSVGSymbolsString(resolve('src/icons'))
+        },
+        tags: [
+          {
+            injectTo: 'body-prepend',
+            tag: 'div',
+            attrs: {
+              id: 'app'
+            }
+          }
+        ]
+      }
+    })],
     server: {
       proxy: {
         '/api': {
           target,
           changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ''),
+          rewrite: (path: string) => path.replace(/^\/api/, ''),
         },
       },
     },
