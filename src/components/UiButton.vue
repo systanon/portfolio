f5673ad5b601183d9e7f5f65b26da010b4b47d4f
Serhii <tustanovskiis@gmail.com>
2025-06-19 13:24:32 +0200

    feature: create buttons components

---
+++ src/components/UiButton.vue
@@ -0,0 +1,52 @@
+<template>
+  <button :disabled="disabled" class="ui-button" :type="type">
+    <slot name="prepend"></slot>
+    <span class="ui-button__content">
+      <slot name="default">
+        {{ label }}
+      </slot>
+    </span>
+    <slot name="append"></slot>
+  </button>
+</template>
+
+<script lang="ts">
+  import { defineComponent, type PropType } from 'vue';
+
+  export default defineComponent({
+    name: 'UiButton',
+    props: {
+      label: {
+        type: String,
+        default: '',
+      },
+      disabled: {
+        type: Boolean,
+        default: false,
+      },
+      type: {
+        type: String as PropType<'button' | 'submit' | 'reset'>,
+        default: 'button',
+      },
+    },
+  });
+</script>
+
+<style lang="scss" scoped>
+  .ui-button {
+    background: transparent;
+    border: none;
+    color: transparent;
+    text-align: center;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+    &:hover {
+      opacity: 1;
+    }
+    &:disabled {
+      opacity: 0.2;
+      pointer-events: none;
+    }
+  }
+</style>
---
+++ src/components/UiIcon.vue
@@ -0,0 +1,31 @@
+<template>
+  <svg class="ui-icon">
+    <use :xlink:href="localHref" height="100%" width="100%"></use>
+  </svg>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue';
+  const DEFAULT_ICON_NAME = 'default';
+  export default defineComponent({
+    name: 'UIIcon',
+    props: {
+      href: { type: String },
+      name: { type: String },
+    },
+    computed: {
+      localHref() {
+        return this.href || `#${this.name || DEFAULT_ICON_NAME}`;
+      },
+    },
+  });
+</script>
+
+<style lang="scss" scoped>
+  .ui-icon {
+    height: 1.5rem;
+    width: 1.5rem;
+    color: var(--text-color-primary);
+    fill: currentColor;
+  }
+</style>
