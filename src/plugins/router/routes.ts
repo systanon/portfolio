7024eb79f8bd83d98ec21fc311bb2f2b8b3b24e3
Serhii <tustanovskiis@gmail.com>
2025-06-19 22:53:41 +0200

    feat: implement default layout

--- src/plugins/router/routes.ts
+++ src/plugins/router/routes.ts
@@ -1,41 +1,46 @@
 import type { RouteRecordRaw } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
+import DefaultLayout from '@/layouts/DefaultLayout.vue'
 
 export const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    name: 'Home',
-    meta: { accessMode: "public" },
-    component: HomeView,
-  },
-  {
-    path: '/todos',
-    name: 'TodoList',
-    meta: { accessMode: "public" },
-    component: () => import('@/views/TodosView.vue'),
-  },
-  {
-    path: '/sign-in',
-    name: 'SignIn',
-    meta: { accessMode: "only-for-unauthorized" },
-    component: () => import('@/views/SignInView.vue'),
-  },
-  {
-    path: '/sign-up',
-    name: 'SignUp',
-    meta: { accessMode: "only-for-unauthorized" },
-    component: () => import('@/views/SignUpView.vue'),
-  },
-  {
-    path: '/profile',
-    name: 'Profile',
-    meta: { accessMode: "private" },
-    component: () => import('@/views/ProfileView.vue'),
-  },
+    path: '',
+    name: 'DefaultLayout',
+    component: DefaultLayout,
+    children: [{
+      path: '/',
+      name: 'Home',
+      meta: { accessMode: "public" },
+      component: HomeView,
+    }, {
+      path: '/todos',
+      name: 'TodoList',
+      meta: { accessMode: "public" },
+      component: () => import('@/views/TodosView.vue'),
+    },
     {
-    path: '/notes',
-    name: 'Notes',
-    meta: { accessMode: "private" },
-    component: () => import('@/views/NotesView.vue'),
-  },
-]
+      path: '/sign-in',
+      name: 'SignIn',
+      meta: { accessMode: "only-for-unauthorized" },
+      component: () => import('@/views/SignInView.vue'),
+    },
+    {
+      path: '/sign-up',
+      name: 'SignUp',
+      meta: { accessMode: "only-for-unauthorized" },
+      component: () => import('@/views/SignUpView.vue'),
+    },
+    {
+      path: '/profile',
+      name: 'Profile',
+      meta: { accessMode: "private" },
+      component: () => import('@/views/ProfileView.vue'),
+    },
+    {
+      path: '/notes',
+      name: 'Notes',
+      meta: { accessMode: "private" },
+      component: () => import('@/views/NotesView.vue'),
+    },
+    ]
+  }]
