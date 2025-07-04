457fa2ca89ddfff7d0da0aece499cfb6be16e529
Serhii <tustanovskiis@gmail.com>
2025-07-04 17:40:19 +0200

    fix: logout

--- src/components/NavigationMenu.vue
+++ src/components/NavigationMenu.vue
@@ -3,41 +3,46 @@
     <RouterLink v-for="{ path, text } in navigationList" :key="path" :to="path">
       {{ text }}
     </RouterLink>
-    <UiButton label="Logout" v-if="isLogged" />
+    <UiButton label="Logout" v-if="isLogged" @click="logout" />
   </nav>
 </template>
 
 <script lang="ts">
-  import { defineComponent } from 'vue';
-  import { byAuthorized, mainMenu, type NavigationMenu } from '@/config/main-menu';
-  import { application } from '@/application';
-  import UiButton from './UiButton.vue';
+import { defineComponent } from 'vue'
+import { byAuthorized, mainMenu, type NavigationMenu } from '@/config/main-menu'
+import { application } from '@/application'
+import UiButton from './UiButton.vue'
 
-  export default defineComponent({
-    name: 'NavigationMenu',
-    components: { UiButton },
-    computed: {
-      isLogged() {
-        return application.isLogged;
-      },
-      navigationList(): NavigationMenu {
-        return mainMenu.filter(byAuthorized(this.isLogged));
-      },
+export default defineComponent({
+  name: 'NavigationMenu',
+  components: { UiButton },
+  computed: {
+    isLogged() {
+      return application.isLogged
     },
-  });
+    navigationList(): NavigationMenu {
+      return mainMenu.filter(byAuthorized(this.isLogged))
+    },
+  },
+  methods: {
+    logout() {
+      application.logout()
+    },
+  },
+})
 </script>
 <style scoped lang="scss">
-  .navigation-menu {
-    display: flex;
-    align-content: center;
-    justify-content: flex-end;
-    align-items: center;
-    gap: 25px;
-    margin: 0;
-    padding: 0;
+.navigation-menu {
+  display: flex;
+  align-content: center;
+  justify-content: flex-end;
+  align-items: center;
+  gap: 25px;
+  margin: 0;
+  padding: 0;
 
-    :deep(.router-link-exact-active) {
-      color: orange;
-    }
+  :deep(.router-link-exact-active) {
+    color: orange;
   }
+}
 </style>
