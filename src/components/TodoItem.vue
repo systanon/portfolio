c5b4ddd3ea489617c93e8e54dc703239f0152f53
Serhii <tustanovskiis@gmail.com>
2025-07-04 14:51:38 +0200

    fix: todo bg

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -17,8 +17,12 @@
       <p class="todo-item__description">{{ todo.description }}</p>
     </div>
     <div class="todo-item__menu">
-      <UiButtonIcon class="todo-item__menu-open" iconName="arrow-up-left" @click="toggleMenu" />
-      <div v-if="menuOpen"   class="todo-item__menu-actions">
+      <UiButtonIcon
+        class="todo-item__menu-open"
+        iconName="arrow-up-left"
+        @click="toggleMenu"
+      />
+      <div v-if="menuOpen" class="todo-item__menu-actions">
         <UiButtonIcon
           class="todo-item__menu-item"
           style="--i: 2"
@@ -37,86 +41,87 @@
 </template>
 
 <script lang="ts" setup>
-  import type { Todo } from '@/types/todo';
-  import UiButtonIcon from '@/components/UiButtonIcon.vue';
-  import { defineProps, ref, type Ref } from 'vue';
-  defineProps<{
-    todo: Todo;
-  }>();
+import type { Todo } from '@/types/todo'
+import UiButtonIcon from '@/components/UiButtonIcon.vue'
+import { defineProps, ref, type Ref } from 'vue'
+defineProps<{
+  todo: Todo
+}>()
 
-  const menuOpen: Ref<boolean> = ref(false);
-  const toggleMenu = () => (menuOpen.value = !menuOpen.value);
+const menuOpen: Ref<boolean> = ref(false)
+const toggleMenu = () => (menuOpen.value = !menuOpen.value)
 </script>
 
 <style scoped lang="scss">
-  .todo-item {
-    position: relative;
-    padding: rem(15) rem(30) rem(15) rem(15);
-    border-radius: 1rem;
-    border: transparent solid 1px;
+.todo-item {
+  position: relative;
+  padding: rem(15) rem(30) rem(15) rem(15);
+  border-radius: 1rem;
+  border: transparent solid 1px;
+  display: flex;
+  gap: 1rem;
+  width: 30rem;
+  height: 10rem;
+  background-color: var(--bg-primary);
+  color: var(--text-color-secondary);
+  overflow: hidden;
+  &__checked {
     display: flex;
-    gap: 1rem;
-    width: 30rem;
-    height: 10rem;
-    background-color: var(--todo-bg-primary);
-    color: var(--text-color-secondary);
-    overflow: hidden;
-    &__checked {
-      display: flex;
-      align-items: center;
-      flex: 0 0;
+    align-items: center;
+    flex: 0 0;
+  }
+  &__info {
+    flex: 1 1;
+  }
+  &__menu {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    &-open {
+      position: relative;
+      z-index: 2;
     }
-    &__info {
-      flex: 1 1;
-    }
-    &__menu {
+    &:before {
+      content: '';
       position: absolute;
-      bottom: 0;
-      right: 0;
-      &-open {
-        position: relative;
-        z-index: 2;
-      }
-      &:before {
-        content: '';
-        position: absolute;
-        width: 200%;
-        height: 200%;
-        border-radius: 50%;
-        background: $bg-menu-secondary;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        z-index: 2;
-      }
-      &-actions {
-        position: absolute;
-        width: 200px;
-        height: 200px;
-        border-radius: 50%;
-        z-index: 1;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        background: $bg-menu-tertiary;
-        border-radius: 50%;
-      }
-      &-item {
-        --angle: calc(var(--i) * 150deg);
-        position: absolute;
-        top: 18%;
-        left: 16%;
-        transform: rotate(var(--angle)) translate(2rem) rotate(calc(var(--angle) * -1));
-        transition: transform 0.3s ease;
-        z-index: 10;
-      }
+      width: 200%;
+      height: 200%;
+      border-radius: 50%;
+      background: $bg-menu-secondary;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      z-index: 2;
     }
-
-    &._checked {
-      border-color: var(--todo-checked);
+    &-actions {
+      position: absolute;
+      width: 200px;
+      height: 200px;
+      border-radius: 50%;
+      z-index: 1;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      background: $bg-menu-tertiary;
+      border-radius: 50%;
     }
-    :deep(.ui-icon) {
-      color: $icon-color-primary;
+    &-item {
+      --angle: calc(var(--i) * 150deg);
+      position: absolute;
+      top: 18%;
+      left: 16%;
+      transform: rotate(var(--angle)) translate(2rem)
+        rotate(calc(var(--angle) * -1));
+      transition: transform 0.3s ease;
+      z-index: 10;
     }
   }
+
+  &._checked {
+    border-color: var(--todo-checked);
+  }
+  :deep(.ui-icon) {
+    color: $icon-color-primary;
+  }
+}
 </style>
