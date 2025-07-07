113180cd95fbd98fca61029641a45140f3d53164
Serhii <tustanovskiis@gmail.com>
2025-07-07 21:26:46 +0200

    feat(UiButtonIcon): added text

--- src/components/UiButtonIcon.vue
+++ src/components/UiButtonIcon.vue
@@ -1,44 +1,46 @@
 <template>
   <button :disabled="disabled" class="ui-button-icon">
+    <slot name="prepend"></slot>
     <UIIcon :name="iconName" />
+    <slot name="append"></slot>
   </button>
 </template>
 
 <script lang="ts">
-  import { defineComponent } from 'vue';
-  import UIIcon from '@/components/UiIcon.vue'
+import { defineComponent } from 'vue'
+import UIIcon from '@/components/UiIcon.vue'
 
-  export default defineComponent({
-    components: { UIIcon },
-    name: 'UiButtonIcon',
-    props: {
-      value: {
-        type: [Boolean, String, Number],
-        default: false,
-      },
-      iconName: {
-        type: String,
-      },
-      disabled: {
-        type: Boolean,
-        default: false,
-      },
+export default defineComponent({
+  components: { UIIcon },
+  name: 'UiButtonIcon',
+  props: {
+    value: {
+      type: [Boolean, String, Number],
+      default: false,
     },
-  });
+    iconName: {
+      type: String,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+})
 </script>
 
 <style lang="scss" scoped>
-  .ui-button-icon {
-    padding: 0.5rem;
-    border: none;
-    display: inline-flex;
-    align-items: center;
-    cursor: pointer;
-    position: relative;
-    overflow: hidden;
-    background-color: transparent;
-    &:disabled {
-      pointer-events: none;
-    }
+.ui-button-icon {
+  padding: 0.5rem;
+  border: none;
+  display: inline-flex;
+  align-items: center;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+  background-color: transparent;
+  &:disabled {
+    pointer-events: none;
   }
+}
 </style>
