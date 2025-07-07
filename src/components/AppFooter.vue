e26e7ceda8034d2330b56ea9356c9c8c2ba650e1
Serhii <tustanovskiis@gmail.com>
2025-07-07 21:26:03 +0200

    fix(AppFooter): position

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -19,12 +19,11 @@ defineProps<{
 .app-footer {
   position: relative;
   width: 100%;
-  height: 300px;
   overflow: hidden;
   &:before {
     background-image: url('@/assets/home-img/layer 1.png');
     content: '';
-    position: absolute;
+    position: fixed;
     background-size: cover;
     object-fit: contain;
     background-position: center;
@@ -32,10 +31,11 @@ defineProps<{
     height: 100%;
     z-index: 1;
     left: -15%;
-    bottom: -10%;
+    bottom: -25%;
     transform: translate(var(--x), var(--y));
     transition: transform 0.1s ease-out;
     will-change: transform;
+    z-index: -4;
   }
 }
 </style>
