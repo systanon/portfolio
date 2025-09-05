5ccba451bcf18c3a4e3f37427a9cc43ea52ecce5
Serhii <tustanovskiis@gmail.com>
2025-09-05 21:03:39 +0200

    feat: created ui

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <script lang="ts">
-  import { defineComponent } from 'vue';
-  import GlobalLoader from '@/components/GlobalLoader.vue';
-  import { useWindowResize } from './composables/useWindowResize';
+import { defineComponent } from 'vue'
+import GlobalLoader from '@/components/GlobalLoader.vue'
+import Notifications from '@/components/Notifications.vue'
+import { useWindowResize } from './composables/useWindowResize'
 
-  export default defineComponent({
-    components: {
-      GlobalLoader,
-    },
-    setup() {
-      useWindowResize();
-    },
-  });
+export default defineComponent({
+  components: {
+    GlobalLoader,
+    Notifications,
+  },
+  setup() {
+    useWindowResize()
+  },
+})
 </script>
 
 <template>
   <router-view />
   <GlobalLoader />
+  <Notifications />
 </template>
---
+++ src/components/Notifications.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="notification-messages">
+    <div
+      v-for="[id, n] in notifications"
+      :key="id"
+      :class="['notification-messages__item', n.type]"
+    >
+      <UiLittleKip :text="n.message" />
+
+      <UiButtonIcon
+        class="notification-messages__item-action"
+        icon-name="close-square"
+        @click="notifications.delete(id)"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { application } from '@/application'
+import UiButtonIcon from './UiButtonIcon.vue'
+import UiLittleKip from './UiLittleKip.vue'
+
+const notifications = application.notifications
+</script>
+<style lang="scss" scoped>
+.notification-messages {
+  position: fixed;
+  bottom: 1rem;
+  right: 1rem;
+  z-index: 50;
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  &__item {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0.75rem 1rem;
+    border-radius: 0.5rem;
+    min-width: 250px;
+    max-width: 350px;
+
+    &.success {
+      background-color: var(--success);
+    }
+
+    &.error {
+      background-color: var(--error);
+    }
+
+    &.info {
+      background-color: var(--info);
+    }
+  }
+}
+</style>
