5c263d82683cdc3c3f8a9f0ac74d6930551f60ab
Serhii <tustanovskiis@gmail.com>
2025-09-05 23:14:10 +0200

    feat: added route confirm email

---
+++ src/views/VerifyEmailView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="confirm-page">
+    <h2 class="confirm-page__title">Verification email page</h2>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+
+import { application } from '@/application'
+import type { ConfirmQuery } from '@/types/auth'
+
+const route = useRoute()
+
+const token: ConfirmQuery = {
+  token: typeof route.query.token === 'string' ? route.query.token : '',
+}
+
+onMounted(() => {
+  application.confirmEmail(token)
+})
+</script>
+
+<style scoped lang="scss">
+.confirm-page {
+  text-align: center;
+  &__title {
+    color: var(--text-color-primary);
+  }
+}
+</style>
