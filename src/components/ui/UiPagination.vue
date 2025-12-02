3c42163eb2c6de909a16a2e835ef362dfe38a5e1
Serhii <tustanovskiis@gmail.com>
2025-12-02 11:17:25 +0100

    feat: created adaptive paginations

--- src/components/ui/UiPagination.vue
+++ src/components/ui/UiPagination.vue
@@ -8,12 +8,40 @@
     />
 
     <UiButton
-      v-for="_page in pages"
+      v-if="pages > VISIBLE_PAGES"
+      :class="['ui-pagination__buttons', { _active: page === 1 }]"
+      @click="$emit('btnPage', 1)"
+      :label="'1'"
+    />
+
+    <span
+      v-if="pages > VISIBLE_PAGES && page > range + 2"
+      class="ui-pagination__dots"
+      >...</span
+    >
+
+    <UiButton
+      v-for="_page in visiblePages"
       :key="_page"
       :class="['ui-pagination__buttons', { _active: page === _page }]"
       @click="$emit('btnPage', _page)"
       :label="`${_page}`"
     />
+
+    <span
+      v-if="pages > VISIBLE_PAGES && page < pages - (range + 1)"
+      label="..."
+      class="ui-pagination__dots"
+      >...</span
+    >
+
+    <UiButton
+      v-if="pages > VISIBLE_PAGES"
+      :class="['ui-pagination__buttons', { _active: page === pages }]"
+      @click="$emit('btnPage', pages)"
+      :label="`${pages}`"
+    />
+
     <UiButtonIcon
       @click="$emit('latestPage')"
       :disabled="page >= pages"
@@ -24,22 +52,43 @@
 </template>
 
 <script lang="ts" setup>
+import { computed } from 'vue'
 import UiButtonIcon from '@/components/ui/buttons/UiButtonIcon.vue'
 import UiButton from '@/components/ui/buttons/UiButton.vue'
+
+const VISIBLE_PAGES = 7
 
 interface Pagination {
   page: number
   pages: number
+  range?: number
 }
 defineOptions({
   name: 'UiPagination',
 })
 
-withDefaults(defineProps<Pagination>(), {
+const props = withDefaults(defineProps<Pagination>(), {
   page: 1,
   pages: 1,
+  range: 2,
+})
+
+const visiblePages = computed(() => {
+  if (props.pages <= VISIBLE_PAGES) {
+    return Array.from({ length: props.pages }, (_, i) => i + 1)
+  }
+
+  const start = Math.max(2, props.page - props.range)
+  const end = Math.min(props.pages - 1, props.page + props.range)
+
+  const range: number[] = []
+  for (let i = start; i <= end; i++) {
+    range.push(i)
+  }
+  return range
 })
 </script>
+
 <style lang="scss" scoped>
 .ui-pagination {
   width: 100%;
@@ -54,6 +103,13 @@ withDefaults(defineProps<Pagination>(), {
         color: var(--active-primary);
       }
     }
+  }
+  &__dots {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    font-size: rem(40);
+    color: var(--text-active-primary);
   }
   :deep(.ui-button__content) {
     font-size: 1.5rem;
