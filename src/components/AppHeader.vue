fe6c1be2e6d27ffff024666a8f683b5eb94d3c60
Serhii <tustanovskiis@gmail.com>
2025-12-27 14:43:02 +0100

    feature: create logo

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,6 +2,7 @@
 import NavigationMenu from '@/components/NavigationMenu.vue'
 import NavigationMenuMobile from './NavigationMenuMobile.vue'
 import { useInjectWindowResize } from '@/composables/useWindowResize'
+import Logo from '@/components/Logo.vue'
 
 const { isTablet, isMobile } = useInjectWindowResize()
 </script>
@@ -9,22 +10,27 @@ const { isTablet, isMobile } = useInjectWindowResize()
 <template>
   <header class="app-header">
     <div class="container">
-      <NavigationMenuMobile v-if="isMobile || isTablet" />
-      <NavigationMenu v-else />
+      <div class="app-header__wrapper">
+        <Logo />
+        <NavigationMenuMobile v-if="isMobile || isTablet" />
+        <NavigationMenu v-else />
+      </div>
     </div>
   </header>
 </template>
 
 <style scoped lang="scss">
 .app-header {
-  display: flex;
-  gap: 15px;
-  padding: 1rem;
-  justify-content: end;
+  padding: rem(16);
+  &__wrapper {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
 }
 @include media-query('tablet') {
   .app-header {
-    padding: 2rem;
+    padding: rem(32);
   }
 }
 </style>
