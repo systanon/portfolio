d32fba8882490f7484d7d1143c128e4d88c2a5c0
Serhii <tustanovskiis@gmail.com>
2026-01-19 17:14:44 +0100

    feat(notes): used ws

--- src/plugins/store/notes.ts
+++ src/plugins/store/notes.ts
@@ -5,70 +5,86 @@ import { AppError } from '@/types/app-errors';
 import type { CreateNoteDTO, Note, UpdateNoteDTO } from '@/types/notes';
 import type { GetAllParams } from '@/types/app.types';
 import type { Application } from '@/application/application';
+import { errorMsg } from '@/helpers/formatErrorMsg';
+import type { WSMessage } from '@/application/services/ws.service';
 
 
 export const useNotesStore = defineStore('notes', () => {
 
-  const notes = ref<Note[]>([])
+
   const notesMap = ref(new Map<number, Note>())
   const total = ref<number>(0)
   const pages = ref<number>(1)
 
   const application = inject('application') as Application
 
+  function messageHandler(message: WSMessage) {
+    switch (message.event) {
+      case 'create':
+        _create(message.data)
+        break
+      case 'update':
+        _update(message.data)
+        break
+      case 'delete':
+        _delete(message.data)
+        break
+    }
+  }
+
   async function getAll(params: GetAllParams) {
     try {
       const { data, total: _total, pages: _pages } = await application.getAllNotes(params)
-      notes.value = data
       notesMap.value = new Map(data.map(note => [note.id, note]))
       total.value = _total
       pages.value = _pages
     } catch (error) {
-      console.error(error)
+      notesMap.value = new Map()
+      total.value = 0
+      pages.value = 1
+      application.notify('error', errorMsg(error))
     }
   }
 
+  function _update(
+    data: any,
+  ): void {
+    notesMap.value.set(data.id, data)
+  }
+
+  function _create(
+    data: any,
+  ): void {
+    notesMap.value.set(data.id, data)
+  }
+
+  function _delete(
+    id: number,
+  ): void {
+    notesMap.value.delete(id)
+  }
+
+
   async function update(_id: number, payload: UpdateNoteDTO) {
-    const _payload = { ...payload }
     const res = await application.updateNote(_id, payload)
     if (res instanceof AppError) {
-      console.warn(res)
-    } else {
-      const note = notes.value.find(t => t.id === _id)
-      if (note) Object.assign(note, _payload)
-
-      const mapTodo = notesMap.value.get(_id)
-      mapTodo && Object.assign(mapTodo, _payload)
+      return res
     }
   }
 
   async function create(payload: CreateNoteDTO) {
     const res = await application.createNote(payload)
     if (res instanceof AppError) {
-      console.warn(res)
-    } else {
-      const note = await application.getOneNote(res)
-      if (note instanceof AppError) {
-        console.warn(res)
-      } else {
-        notes.value.push(note)
-        notesMap.value.set(note.id, note)
-      }
+      return res
     }
   }
 
   async function remove(id: number) {
     const res = await application.deleteNote(id)
     if (res instanceof AppError) {
-      console.warn(res)
-    } else {
-      const index = notes.value.findIndex((note) => note.id === id)
-      if (index !== -1) {
-        notes.value.splice(index, 1)
-      }
-      notesMap.value.delete(id)
+      return res
     }
   }
 
-  return { getAll, notes, notesMap, update, create, remove, total, pages }
+  return { getAll, notesMap, update, create, remove, total, pages, messageHandler }
 })
