45ad459e7c4b98e2b9b8e197e05a0e6c7227a9be
Serhii <tustanovskiis@gmail.com>
2026-01-25 22:50:16 +0100

    feat(NavigationMenuMobile.vue): changed by new design

--- src/components/NavigationMenuMobile.vue
+++ src/components/NavigationMenuMobile.vue
@@ -1,64 +1,61 @@
-<script lang="ts">
-import { defineComponent, type Directive } from 'vue'
-import { vOnClickOutside as baseOnClickOutside } from '@vueuse/components'
-import UIButtonIcon from '@/components/ui/buttons/UiButtonIcon.vue'
-import NavigationMenu from './NavigationMenu.vue'
-
-const vOnClickOutside: Directive = baseOnClickOutside
-
-export default defineComponent({
-  name: 'NavigationMenuMobile',
-  components: {
-    UIButtonIcon,
-    NavigationMenu,
-  },
-  directives: {
-    onClickOutside: vOnClickOutside,
-  },
-  data() {
-    return {
-      isNavOpen: false,
-    }
-  },
-  methods: {
-    toggleNav() {
-      this.isNavOpen = !this.isNavOpen
-    },
-    close() {
-      this.isNavOpen = false
-    },
-  },
-})
-</script>
-
 <template>
-  <nav class="app-nav">
+  <div class="app-nav">
     <UIButtonIcon
       class="app-nav__burger-menu"
       @click="toggleNav"
       iconName="burger-menu"
     />
-  </nav>
-  <aside
-    class="app-navigation"
-    :class="{ open: isNavOpen }"
-    v-on-click-outside="close"
-  >
-    <UIButtonIcon @click="toggleNav" iconName="close-square" />
-    <NavigationMenu @click="close" />
-  </aside>
+  </div>
+  <Transition name="slide">
+    <aside v-if="isNavOpen" class="app-navigation" v-on-click-outside="close">
+      <UIButtonIcon @click="toggleNav" iconName="close-square" />
+      <nav class="app-navigation__menu">
+        <AppLink
+          v-for="{ path, text, routeName } in menuList"
+          :key="path"
+          :to="{ name: routeName }"
+          inactive-class="link"
+          exactActiveClass="link--active"
+          @click="close"
+        >
+          {{ text }}
+        </AppLink>
+      </nav>
+    </aside>
+  </Transition>
 </template>
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+import type { Directive } from 'vue'
+import { vOnClickOutside as baseOnClickOutside } from '@vueuse/components'
+import { byAuthorized, mainMenu } from '@/config/main-menu'
+import UIButtonIcon from '@/components/ui/buttons/UiButtonIcon.vue'
+import AppLink from './AppLink.vue'
+import { useLogged } from '@/composables/useLogged'
+
+const vOnClickOutside: Directive = baseOnClickOutside
+
+const isNavOpen = ref(false)
+
+const { isLogged } = useLogged()
+
+const toggleNav = () => {
+  isNavOpen.value = !isNavOpen.value
+}
+
+const close = () => {
+  isNavOpen.value = false
+}
+
+const menuList = computed(() => {
+  return mainMenu.filter(byAuthorized(isLogged.value))
+})
+</script>
 
 <style lang="scss" scoped>
 .app-nav {
   display: flex;
   justify-content: end;
-
-  &__burger-menu {
-    background-color: transparent;
-    transition: opacity 0.3s ease-in-out;
-    color: $text-color-primary;
-  }
 }
 
 .app-navigation {
@@ -73,11 +70,9 @@ export default defineComponent({
   position: fixed;
   top: 0;
   left: 0;
-  transform: translateX(-100%);
-  transition: transform 0.3s ease-in-out;
   z-index: 100;
 
-  .navigation-menu {
+  &__menu {
     width: 100%;
     color: $text-color-primary;
     display: flex;
@@ -85,14 +80,24 @@ export default defineComponent({
     align-items: center;
     gap: rem(30);
     font-size: rem(22);
-
-    &__item {
-      cursor: pointer;
-    }
   }
 }
 
-.app-navigation.open {
+.slide-enter-from {
+  transform: translateX(-100%);
+}
+
+.slide-enter-active,
+.slide-leave-active {
+  transition: transform 0.3s ease-in-out;
+}
+
+.slide-enter-to,
+.slide-leave-from {
   transform: translateX(0);
+}
+
+.slide-leave-to {
+  transform: translateX(-100%);
 }
 </style>
