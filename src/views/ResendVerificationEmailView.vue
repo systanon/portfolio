4c90e1487373f9edb4cedece815c9be47254148a
Serhii <tustanovskiis@gmail.com>
2026-02-06 13:03:27 +0100

    feat(ResendVerificationEmailView): implement countdown

--- src/views/ResendVerificationEmailView.vue
+++ src/views/ResendVerificationEmailView.vue
@@ -8,20 +8,35 @@
         placeholder="Enter email"
         :validation="v$.email"
       />
-      <UiButton type="submit" label="Submit" />
+      <UiButton type="submit" label="Submit" :disabled="isBlocked" />
+      <ProgressBar
+        v-if="showProgressBar"
+        ref="progressBarRef"
+        :progress="false"
+      >
+        <span>{{ time }}</span>
+      </ProgressBar>
     </form>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, nextTick, onMounted } from 'vue'
 import { application } from '@/application'
 import UiInput from '@/components/ui/fields/UiInput.vue'
 import UiButton from '@/components/ui/buttons/UiButton.vue'
 import { useValidationRules } from '@/composables/useValidationRules'
 import useVuelidate from '@vuelidate/core'
+import ProgressBar, {
+  type IProgressBar,
+} from '@/components/animation/ProgressBar.vue'
+import { AppRateLimitError } from '@/types/app-errors'
 
 const email = ref<string>('')
+const isBlocked = ref<boolean>(false)
+const showProgressBar = ref<boolean>(false)
+const progressBarRef = ref<IProgressBar | null>(null)
+const time = ref('0:00')
 
 const { emailRules } = useValidationRules()
 
@@ -33,14 +48,39 @@ const v$ = useVuelidate(rules, {
   email,
 })
 
+const formatTime = (sec: number) => {
+  const m = Math.floor(sec / 60)
+  const s = Math.floor(sec % 60)
+  return `${m}:${s.toString().padStart(2, '0')}`
+}
+
 const submitHandler = async () => {
   const isValid = await v$.value.$validate()
   if (!isValid) return
+  isBlocked.value = true
   const payload = {
     email: email.value,
   }
 
-  await application.resendConfirmEmail(payload)
+  const res = await application.resendConfirmEmail(payload)
+  if (res instanceof AppRateLimitError) {
+    showProgressBar.value = true
+    await nextTick()
+    progressBarRef.value?.play(
+      res.retryAfter,
+      () => {
+        isBlocked.value = false
+        showProgressBar.value = false
+        time.value = '0:00'
+      },
+      (tween) => {
+        const remaining = tween.duration() - tween.time()
+        time.value = formatTime(remaining)
+      },
+    )
+  } else {
+    isBlocked.value = false
+  }
 }
 </script>
 
@@ -54,12 +94,13 @@ const submitHandler = async () => {
   }
   &__form {
     display: flex;
+    position: relative;
     flex-direction: column;
     gap: rem(15);
     background-color: var(--bg-primary);
     padding: rem(30);
     border-radius: rem(15);
-    width: rem(500);
+    width: 100%;
     max-width: rem(500);
   }
 }
