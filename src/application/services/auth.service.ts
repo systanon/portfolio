cf16dcfefffb5d726582cc28db0fe76d2cc9d84f
Serhii <tustanovskiis@gmail.com>
2026-03-10 20:02:04 +0100

    feat(auth.service): improve response type definitions

--- src/application/services/auth.service.ts
+++ src/application/services/auth.service.ts
@@ -1,27 +1,18 @@
-import { errorMsg } from '@/helpers/formatErrorMsg'
 import type { HTTPClient } from '@/lib/http.client'
-import {
-  AppError,
-  AppRateLimitError,
-  AppSilentError,
-  AppSuccess,
-  isHttpError,
-  type APIError,
-} from '@/types/app-errors'
+import { AppError, AppRateLimitError, AppSilentError } from '@/types/app-errors'
 import type {
-  AuthResponse,
   ConfirmQuery,
   ForgotPasswordDto,
-  RegistrationResponse,
   ResendConfirmEmailDto,
   ResetPasswordDto,
   SignInDto,
   SignUpDto,
-  SuccessResponse,
   UserProfile,
   UserProfileUpdateInfo,
 } from '@/types/auth'
 import { API_URL } from '@/constants'
+import { AppSuccess } from '@/types/app.types'
+import { Errors } from '@/constants'
 
 export class AuthService {
   private readonly httpClient: HTTPClient
@@ -33,174 +24,184 @@ export class AuthService {
   async registration(dto: SignUpDto): Promise<AppSuccess | AppError> {
     const url = API_URL.auth.sign_up
     const body = JSON.stringify(dto)
-    try {
-      const result = await this.httpClient.jsonDo(url, {
-        method: 'POST',
-        body,
-        credentials: 'include',
-        resource: url,
-        url,
-      })
-      return new AppSuccess<RegistrationResponse>(errorMsg(result))
-    } catch (error) {
-      return new AppError(errorMsg(error))
+    const result = await this.httpClient.jsonDo<null>(url, {
+      method: 'POST',
+      body,
+      credentials: 'include',
+      resource: url,
+      url,
+    })
+    if (result instanceof AppError) {
+      return result
     }
+
+    return new AppSuccess(null, result.message)
   }
 
   async confirmEmail(params: ConfirmQuery): Promise<void | AppError> {
-    try {
-      const response = await this.httpClient.do(API_URL.auth.confirm, {
-        method: 'POST',
-        params,
-      })
-      if (response.ok) {
-        const { access_token } = (await response.json()) as AuthResponse
+    const url = API_URL.auth.confirm
+    const response = await this.httpClient.jsonDo(url, {
+      method: 'POST',
+      params,
+      resource: url,
+      url,
+    })
+    if (response instanceof AppSuccess) {
+      const access_token = response.headers?.get('access_token')
+      if (access_token) {
         localStorage.setItem('access_token', access_token)
       } else {
-        return new AppError(errorMsg(await response.json()))
+        return new AppError(
+          'access token not found',
+          Errors.ACCESS_TOKEN_NOT_FOUND,
+        )
       }
-    } catch (error) {
-      return new AppError(errorMsg(error))
+    } else {
+      return response
     }
   }
   async resendConfirmEmail(
     dto: ResendConfirmEmailDto,
-  ): Promise<SuccessResponse | AppError | AppRateLimitError> {
+  ): Promise<AppSuccess | AppError | AppRateLimitError> {
     const url = API_URL.auth.resendEmail
     const body = JSON.stringify(dto)
-    try {
-      const result: RegistrationResponse = await this.httpClient.jsonDo(url, {
-        method: 'POST',
-        body,
-        resource: url,
-        url,
-      })
-      return result
-    } catch (error: unknown) {
-      if (isHttpError(error)) {
-        if (error.status === 429) {
-          return new AppRateLimitError(errorMsg(error), error.data.retryAfter)
-        }
+    const result = await this.httpClient.jsonDo(url, {
+      method: 'POST',
+      body,
+      resource: url,
+      url,
+    })
+
+    if (result instanceof AppError) {
+      if (result.code === Errors.RATE_LIMIT) {
+        const retry = result.headers!.get('Retry-After')
+        return new AppRateLimitError(result.message, Number(retry))
       }
-      return new AppError(errorMsg(error))
+      return result
     }
+    return result
   }
   async forgotPassword(
     dto: ForgotPasswordDto,
-  ): Promise<SuccessResponse | AppError> {
+  ): Promise<AppSuccess | AppError | AppRateLimitError> {
     const url = API_URL.auth.forgotPass
     const body = JSON.stringify(dto)
-    try {
-      return await this.httpClient.jsonDo(url, {
-        method: 'POST',
-        body,
-        resource: url,
-        url,
-      })
-    } catch (error: unknown) {
-      if (isHttpError(error)) {
-        if (error.status === 429) {
-          return new AppRateLimitError(errorMsg(error), error.data.retryAfter)
-        }
+    const result = await this.httpClient.jsonDo(url, {
+      method: 'POST',
+      body,
+      resource: url,
+      url,
+    })
+    if (result instanceof AppError) {
+      if (result.code === Errors.RATE_LIMIT) {
+        const retry = result.headers!.get('Retry-After')
+        return new AppRateLimitError(result.message, Number(retry))
       }
-      return new AppError(errorMsg(error))
+      return result
     }
+    return result
   }
 
-  async resetPassword(
-    dto: ResetPasswordDto,
-  ): Promise<SuccessResponse | AppError> {
+  async resetPassword(dto: ResetPasswordDto): Promise<AppSuccess | AppError> {
     const url = API_URL.auth.resetPass
     const body = JSON.stringify(dto)
-    try {
-      return await this.httpClient.jsonDo(url, {
-        method: 'POST',
-        body,
-        resource: url,
-        url,
-      })
-    } catch (error) {
-      return new AppError(errorMsg(error))
-    }
+    return await this.httpClient.jsonDo(url, {
+      method: 'POST',
+      body,
+      resource: url,
+      url,
+    })
   }
 
   async authorization(dto: SignInDto): Promise<void | AppError> {
     const url = API_URL.auth.sign_in
     const body = JSON.stringify(dto)
-    try {
-      const result: AuthResponse = await this.httpClient.jsonDo(url, {
-        method: 'POST',
-        body,
-        credentials: 'include',
-        resource: url,
-        url,
-      })
-      localStorage.setItem('access_token', result.access_token)
-    } catch (error) {
-      return new AppError(errorMsg(error))
-    }
-  }
-  async getProfile(): Promise<UserProfile | AppError> {
-    const url = API_URL.auth.profile
-    try {
-      const result = await this.httpClient.jsonDo(url, {
-        method: 'POST',
-        credentials: 'include',
-        resource: url,
-        url,
-      })
-      return result.message
-    } catch (e: unknown) {
-      const error = e as APIError
-      const msg = errorMsg(error.data)
-      if (error.status === 401) {
-        return new AppSilentError(msg)
+    const result = await this.httpClient.jsonDo(url, {
+      method: 'POST',
+      body,
+      credentials: 'include',
+      resource: url,
+      url,
+    })
+
+    if (result instanceof AppSuccess) {
+      const { access_token } = result.data
+      if (access_token) {
+        localStorage.setItem('access_token', access_token)
+      } else {
+        return new AppError(
+          'access token not found',
+          Errors.ACCESS_TOKEN_NOT_FOUND,
+        )
       }
-      return new AppError(msg)
+    } else {
+      return result
     }
   }
-  async updateProfile(dto: UserProfileUpdateInfo): Promise<AppError | string> {
+  async getProfile(): Promise<UserProfile | AppError | AppSilentError> {
+    const url = API_URL.auth.profile
+    const result = await this.httpClient.jsonDo<UserProfile>(url, {
+      method: 'POST',
+      credentials: 'include',
+      resource: url,
+      url,
+    })
+    if (result instanceof AppSuccess) {
+      return result.data
+    } else {
+      if (result.code === Errors.UNAUTHORIZED) {
+        return new AppSilentError(result.message)
+      }
+    }
+    return result
+  }
+  async updateProfile(
+    dto: UserProfileUpdateInfo,
+  ): Promise<AppError | AppSuccess> {
     const url = API_URL.auth.profile
     const body = JSON.stringify(dto)
-    try {
-      const result = await this.httpClient.jsonDo(url, {
-        method: 'PATCH',
-        credentials: 'include',
-        resource: url,
-        body,
-        url,
-      })
-      return result.msg
-    } catch (error) {
-      return new AppError(errorMsg(error))
-    }
+    return await this.httpClient.jsonDo(url, {
+      method: 'PATCH',
+      credentials: 'include',
+      resource: url,
+      body,
+      url,
+    })
   }
-  async refresh(): Promise<void | AppError> {
+  async refresh(): Promise<AppSuccess | void> {
     const url = API_URL.auth.refresh
-    try {
-      const result = await this.httpClient.jsonDo(url, {
-        method: 'POST',
-        credentials: 'include',
-        resource: url,
-        url,
-      })
-      localStorage.setItem('access_token', result.access_token)
-    } catch (error) {
-      throw new AppError(errorMsg(error))
+    const result = await this.httpClient.jsonDo(url, {
+      method: 'POST',
+      credentials: 'include',
+      resource: url,
+      url,
+    })
+    if (result instanceof AppSuccess) {
+      const { access_token } = result.data
+      if (access_token) {
+        localStorage.setItem('access_token', access_token)
+      } else {
+        throw new AppError(
+          'access token not found',
+          Errors.ACCESS_TOKEN_NOT_FOUND,
+        )
+      }
+    } else {
+      throw result
     }
   }
   async logout(): Promise<void | AppError> {
     const url = API_URL.auth.logout
-    try {
-      await this.httpClient.jsonDo(url, {
-        method: 'POST',
-        credentials: 'include',
-        resource: url,
-        url,
-      })
+    const result = await this.httpClient.jsonDo(url, {
+      method: 'POST',
+      credentials: 'include',
+      resource: url,
+      url,
+    })
+    if (result instanceof AppSuccess) {
       localStorage.removeItem('access_token')
-    } catch (error) {
-      throw new AppError(errorMsg(error))
+    } else {
+      return result
     }
   }
 }
