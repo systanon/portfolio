9b00dac340a008340fff595504048cab8f38e149
Serhii <tustanovskiis@gmail.com>
2026-05-05 08:11:56 +0200

    fix: types and add constants

--- src/types/i18n.ts
+++ src/types/i18n.ts
@@ -1,8 +1,9 @@
+import type { SUPPORT_LOCALES } from '@/constants'
 import en from '@/i18n/locales/en.json'
 
 export type MessageSchema = typeof en
-export type SupportedLocale = 'en' | 'pl' | 'ua'
-
-export const SUPPORT_LOCALES: SupportedLocale[] = ['en', 'pl', 'ua']
-export const DEFAULT_LOCALE: SupportedLocale = 'en'
-export type PartialMessages = { [K in SupportedLocale]: MessageSchema }
+export type SupportedLocale = (typeof SUPPORT_LOCALES)[number]
+export type LocaleOption = {
+  value: SupportedLocale
+  label: Uppercase<SupportedLocale>
+}
